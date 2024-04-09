34eef159da5925f15084b78d182664a7ad78ca43
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-09 18:21:50 +0600

    header completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,8 @@
-import Navbar from "../Navbar/Navbar";
-
+import Header from "../Header/Header";
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            <Header></Header>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,7 @@
-import kiit25thLogo from "../../assets/images/KIIT Silver jubilee logo.png";
-import { FaSquarePhone } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
 const Navbar = () => {
     return (
-        <div className="bg-gradient-to-r from-white to-[#5ad18a]">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-between p-7">
-            <div className="mx-auto">
-                <img className="max-w-96" src={kiit25thLogo} alt="kiit logo 25th silver jubilee" />
-            </div>
-            <div className="font-medium flex flex-col gap-2 text-center">
-                <h1 className="text-2xl">KIIT University</h1>
-                <h1 className="text-2xl">International Student Portal</h1>
-                <h2 className="text-lg">(For International Students)</h2>
-            </div>
-            <div className="mx-auto space-y-3">
-                <p className="flex flex-row text-xl"><FaSquarePhone className="mt-1" /> <span className="font-medium ml-2">Call Us :</span><a className="text-black" href="tel:+91 62 9021 9676">+916290219676</a></p>
-                <p className="flex flex-row text-xl"><MdEmail className="mt-1" /> <span className="font-medium ml-2">Contact Us :</span><a className="text-black" href="mailto:internationaloffice@kiit.ac.in">internationaloffice@kiit.ac.in</a></p>
-            </div>
-        </div>
+        <div>
+            
         </div>
     );
 };
