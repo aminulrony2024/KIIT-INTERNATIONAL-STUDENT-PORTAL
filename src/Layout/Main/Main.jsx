eaab35f359413dddf62d6275eb3e7732128f3c1d
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-09 21:05:41 +0600

    bootstrap shouldn't be use

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
