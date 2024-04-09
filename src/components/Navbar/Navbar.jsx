8012d28e175924c187f59c2badf6fdf754610250
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-09 16:22:24 +0600

    kiit logo added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import kiit25thLogo from "../../assets/images/KIIT Silver jubilee logo.png";
+const Navbar = () => {
+    return (
+        <div>
+            <div className="bg-gradient-to-r from-[rgb(98, 209, 137)] to-[rgb(61, 190, 237)]">
+                <img src={kiit25thLogo} alt="kiit logo 25th silver jubilee" />
+            </div>
+            <div>
+                <h1></h1>
+                <h1></h1>
+                <h2></h2>
+            </div>
+            <div>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
