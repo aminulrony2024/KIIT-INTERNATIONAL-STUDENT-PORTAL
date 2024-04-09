713ff50d8687919691e7191a2dd8b3a620222353
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-09 20:51:11 +0600

    react bootstrap added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { router } from "./Routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
