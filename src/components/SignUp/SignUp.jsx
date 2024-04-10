a7ad92ce16aa1e30f5a8dd45ff0f0ea5a696a7d3
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-10 22:15:09 +0600

    register button won't work if password didn't match

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    const [passMatch, setPassMatch] = useState(false);
+    const [password, setPassword] = useState('');
+    const passwordCheck =(e)=>{
+        setPassword(e.target.value);
+        console.log(e.target.value);
+    }
+    const passMatchCheck = e => {
+        const confirmPassword = e.target.value;
+        if(password === confirmPassword)
+        setPassMatch(true);
+        else
+        setPassMatch(false);
+    }
   return (
     <div className="bg-gradient-to-r from-[#62d189] to-sky-300 m-4">
           <form className="card-body">
@@ -8,6 +22,7 @@ const SignUp = () => {
             <div className="form-control">
               <input
                 type="text"
+                name="name"
                 placeholder="Enter your name *"
                 className="input input-bordered"
                 required
@@ -16,6 +31,7 @@ const SignUp = () => {
             <div className="form-control">
               <input
                 type="email"
+                name="email"
                 placeholder="Enter your email *"
                 className="input input-bordered"
                 required
@@ -24,21 +40,25 @@ const SignUp = () => {
             <div className="form-control">
               <input
                 type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
+                onChange={passwordCheck}
               />
             </div>
             <div className="form-control">
               <input
-                type="password *"
+                type="password"
+                name="confirmpassword"
                 placeholder="confirm password *"
                 className="input input-bordered"
                 required
+                onChange={passMatchCheck}
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary bg-sky-500 bg-gradient-to-r from-sky-500 to-blue-700 border-none text-white text-xl">Register</button>
+              <button disabled={!passMatch} className="btn btn-primary bg-sky-500 bg-gradient-to-r from-sky-500 to-blue-700 border-none text-white text-xl">Register</button>
             </div>
             <p className="text-center">Already have an account? <Link to="/login">Log in</Link></p>
           </form>
