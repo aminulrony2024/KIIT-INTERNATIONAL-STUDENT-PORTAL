5137d9840c3c73c70fa6055ad8034d9db60c523e
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-23 22:17:13 +0600

    Admin route created

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,13 +1,16 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
+import useAdmin from "../Hooks/useAdmin";
 
-const AdminRoute = ({children}) => {
-    const { user, loading } = useAuth();
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useAuth();
   const location = useLocation();
-  if (loading) 
-  return <span className="loading loading-dots loading-lg"></span>;
-  if (user?.email) return children;
-  return <Navigate to="/login" state={location.pathname} replace={true}></Navigate>;
+  const [isAdmin,isAdminLoading] = useAdmin();
+  if (loading || isAdminLoading) return <span className="loading loading-dots loading-lg"></span>;
+  if (user?.email && isAdmin) return children;
+  return (
+    <Navigate to="/login" state={location.pathname} replace={true}></Navigate>
+  );
 };
 
 export default AdminRoute;
