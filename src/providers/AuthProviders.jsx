934a94a1c094de7af0b7b42f9c11d43cc43ae977
aminulrony1999 <aminulrony1999@gmail.com>
2024-04-24 13:20:44 +0600

    access token generate request successfully sent to backend

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -7,15 +7,32 @@ import {
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 import { createContext, useEffect, useState } from "react";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 const auth = getAuth(app);
 export const AuthContext = createContext(null);
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      setLoading(false);
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post('/jwt', userInfo)
+            .then(res => {
+                if (res.data.token) {
+                    localStorage.setItem('access-token', res.data.token);
+                    setLoading(false);
+                }
+            })
+    }
+    else {
+        // Remove token (if token stored in the client side from local storage if the user is logged out)
+        localStorage.removeItem('access-token');
+        setLoading(false);
+    }
     });
     return () => {
       return unsubscribe();
