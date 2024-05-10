05ff842b820d74aaf5c3e3f0e5eac93ef1812421
aminulrony1999 <aminulrony1999@gmail.com>
2024-05-10 14:47:37 +0600

    all users page completed

--- src/components/AllUsers/AllUsers.jsx
+++ src/components/AllUsers/AllUsers.jsx
@@ -1,12 +1,125 @@
 import { Helmet } from "react-helmet-async";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
+import { NavLink } from "react-router-dom";
 
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users, refetch } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  const handleMakeAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, make user admin!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+          if (res.data.modifiedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Done!",
+              text: `${user.name} is an Admin Now!`,
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete the user!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "User has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <Helmet>
         <title>KIIT IRO | Users</title>
       </Helmet>
-      <h1>I am from all user</h1>
+      <h1 className="text-2xl font-medium pb-2 border-b border-[#ccc] max-w-5xl">
+        Total User : {users?.length}
+      </h1>
+      <div className="overflow-x-auto mt-8">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <NavLink to={`/home/user/${user._id}`}>{user.name}</NavLink>
+                </td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-lg bg-green-600 hover:bg-[#75b740]"
+                    >
+                      <FaUsers
+                        className="text-white 
+                                        text-2xl"
+                      ></FaUsers>
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
